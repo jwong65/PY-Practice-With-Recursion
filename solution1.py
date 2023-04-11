81c75879836f42ec8abee009b913aa69493afdf3
jwong65 <jason.wong6507@gmail.com>
2023-04-11 19:28:12 -0400

    First two problems completed

--- solution1.py
+++ solution1.py
@@ -1,1 +1,10 @@
 # Write code for algorithm 1 below
+# Direct recursion.
+def postitive(n):
+    if n==0:
+        return
+    else:
+        print(n)
+        postitive(n-1)
+
+# postitive(5)
--- solution2.py
+++ solution2.py
@@ -1,1 +1,10 @@
 # Write code for algorithm 2 below
+# o has to be the n 
+def counts_up(n, o= 1):
+    if (o>n):
+        return
+    else:
+        print (o)
+        # So this would print 1 at first
+        counts_up(n, o+1)
+        # Now there's a not a default argument that increases
