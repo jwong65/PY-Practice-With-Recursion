25f615c354f1619f00a60e9a75cf314bf05765ea
jwong65 <jason.wong6507@gmail.com>
2023-04-11 19:51:46 -0400

    Finished the exponents and palindromes

--- solution4.py
+++ solution4.py
@@ -1,1 +1,7 @@
 # Write code for algorithm 4 below
+def powerof(a, b):
+    return a**b
+
+# ** for the pwower of
+# print(powerof(3, 5)) =243
+# print(powerof(2,4)) = 16
--- solution5.py
+++ solution5.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 5 below
+# Palindrome
+def palindrome(n):
+    # Reversing a string in python
+    reverse = n[::-1]
+    if (reverse.lower() == n.lower()):
+        return True
+    else:
+        return False
+    
+# print(palindrome("radar")) = True
+# print(palindrome("Random")) = False
+# print(palindrome("Radar")) = Now true with the .lower statements
