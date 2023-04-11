658ccec09e48e3b08e8a9b4f0b9cc4ffbe1d0da9
jwong65 <jason.wong6507@gmail.com>
2023-04-11 19:44:20 -0400

    Fibonacci Sequence for solution 3

--- solution3.py
+++ solution3.py
@@ -1,1 +1,20 @@
 # Write code for algorithm 3 below
+# Fibonacci Sequnce?
+
+def fibo(n):
+    # This doesn't work with negatives
+    if (n<=0):
+        return
+    elif(n==1):
+        return n
+    elif(n==2):
+        return 1
+    elif(n==3):
+        return 1
+    else:
+        return fibo(n-1)+fibo(n-2)
+    
+# print(fibo(3)) = 1
+# print(fibo(4)) = 2
+# print (fibo(7)) = 8
+# print(fibo(9)) = 21
